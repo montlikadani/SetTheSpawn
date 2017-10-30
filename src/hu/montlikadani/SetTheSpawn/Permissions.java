46cf4ce632592b5ce32633235b0711ca317eb71a
montlikadani <montlikada@gmail.com>
2017-10-30 13:05:24 +0100

    Create Permissions.java

---
+++ src/hu/montlikadani/SetTheSpawn/Permissions.java
@@ -0,0 +1,22 @@
+package hu.montlikadani.SetTheSpawn;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+	
+	public Permission perm = new Permission("setthespawn.cmd.nocooldown");
+	public Permission perm1 = new Permission("setthespawn.plugininfo");
+	public Permission perm2 = new Permission("setthespawn.help");
+	public Permission perm3 = new Permission("setthespawn.reload");
+	public Permission perm4 = new Permission("setthespawn.plugindisable");
+	public Permission perm5 = new Permission("setthespawn.setspawn");
+	public Permission perm6 = new Permission("setthespawn.spawn.use");
+	public Permission perm7 = new Permission("setthespawn.delspawn");
+	public Permission perm8 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport-on-move.permission"));
+	public Permission perm9 = new Permission("setthespawn.bypass.cooldown");
+	public Permission perm10 = new Permission("setthespawn.cmd.tabcomplete");
+	public Permission perm11 = new Permission("setthespawn.spawn.teleport.all");
+	public Permission perm12 = new Permission("setthespawn.spawn.teleport.other");
+	public Permission perm13 = new Permission("setthespawn.spawn.setup");
+
+}
