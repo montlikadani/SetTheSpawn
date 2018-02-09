6e20e4166d2b6684f52f8a0ada1327e97d241bd9
montlikadani <montlikada@gmail.com>
2018-02-09 16:53:12 +0100

    Added some multiple permissions

--- src/hu/montlikadani/SetTheSpawn/Permissions.java
+++ src/hu/montlikadani/SetTheSpawn/Permissions.java
@@ -11,7 +11,7 @@ public class Permissions {
 	public Permission perm5 = new Permission("setthespawn.setspawn");
 	public Permission perm6 = new Permission("setthespawn.spawn.use");
 	public Permission perm7 = new Permission("setthespawn.delspawn");
-	public Permission perm8 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport-on-move.permission"));
+	public Permission perm8 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport.move.permission"));
 	public Permission perm9 = new Permission("setthespawn.bypass.cooldown");
 	public Permission perm10 = new Permission("setthespawn.cmd.tabcomplete");
 	public Permission perm11 = new Permission("setthespawn.spawn.teleport.all");
@@ -22,5 +22,7 @@ public class Permissions {
 	public Permission perm16 = new Permission("setthespawn.sign.use");
 	public Permission perm17 = new Permission(SetTheSpawn.getInstance().getConfig().getString("spawn-cost.permission"));
 	public Permission perm18 = new Permission(SetTheSpawn.getInstance().getConfig().getString("spawn-cost.sign.permission"));
+	public Permission perm19 = new Permission("setthespawn.countdown.stopall");
+	public Permission perm20 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport.jump.permission"));
 
 }
