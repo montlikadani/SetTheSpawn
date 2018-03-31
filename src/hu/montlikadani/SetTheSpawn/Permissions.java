7b4ee234cb53405d1a71cbfe8a82d2380f499bdc
montlikadani <montlikada@gmail.com>
2018-03-31 17:57:18 +0200

    Added some multiple permission

--- src/hu/montlikadani/SetTheSpawn/Permissions.java
+++ src/hu/montlikadani/SetTheSpawn/Permissions.java
@@ -4,25 +4,30 @@ import org.bukkit.permissions.Permission;
 
 public class Permissions {
 
-	public Permission perm1 = new Permission("setthespawn.plugininfo");
-	public Permission perm2 = new Permission("setthespawn.help");
-	public Permission perm3 = new Permission("setthespawn.reload");
-	public Permission perm4 = new Permission("setthespawn.plugindisable");
-	public Permission perm5 = new Permission("setthespawn.setspawn");
-	public Permission perm6 = new Permission("setthespawn.spawn.use");
-	public Permission perm7 = new Permission("setthespawn.delspawn");
-	public Permission perm8 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport.move.permission"));
-	public Permission perm9 = new Permission("setthespawn.bypass.cooldown");
-	public Permission perm10 = new Permission("setthespawn.cmd.tabcomplete");
-	public Permission perm11 = new Permission("setthespawn.spawn.teleport.all");
-	public Permission perm12 = new Permission("setthespawn.spawn.teleport.other");
-	public Permission perm13 = new Permission("setthespawn.spawn.setup");
-	public Permission perm14 = new Permission("setthespawn.sign.create");
-	public Permission perm15 = new Permission("setthespawn.sign.break");
-	public Permission perm16 = new Permission("setthespawn.sign.use");
-	public Permission perm17 = new Permission(SetTheSpawn.getInstance().getConfig().getString("spawn-cost.permission"));
-	public Permission perm18 = new Permission(SetTheSpawn.getInstance().getConfig().getString("spawn-cost.sign.permission"));
-	public Permission perm19 = new Permission("setthespawn.countdown.stopall");
-	public Permission perm20 = new Permission(SetTheSpawn.getInstance().getConfig().getString("cancel-teleport.jump.permission"));
+	public static final SetTheSpawn plugin = SetTheSpawn.getInstance();
+
+	public static final Permission PINFO = new Permission("setthespawn.plugininfo");
+	public static final Permission HELP = new Permission("setthespawn.help");
+	public static final Permission RELOAD = new Permission("setthespawn.reload");
+	public static final Permission PDISABLE = new Permission("setthespawn.plugindisable");
+	public static final Permission TABCOMP = new Permission("setthespawn.cmd.tabcomplete");
+	public static final Permission SETSPAWN = new Permission("setthespawn.setspawn");
+	public static final Permission SPAWNUSE = new Permission("setthespawn.spawn.use");
+	public static final Permission DELSPAWN = new Permission("setthespawn.delspawn");
+	public static final Permission BYPASSCOOLD = new Permission("setthespawn.bypass.cooldown");
+	public static final Permission SPAWNALL = new Permission("setthespawn.spawn.teleport.all");
+	public static final Permission SPAWNOTHER = new Permission("setthespawn.spawn.teleport.other");
+	public static final Permission SETUP = new Permission("setthespawn.spawn.setup");
+	public static final Permission CREATESIGN = new Permission("setthespawn.sign.create");
+	public static final Permission BREAKSIGN = new Permission("setthespawn.sign.break");
+	public static final Permission USESIGN = new Permission("setthespawn.sign.use");
+	public static final Permission SIGNTPALL = new Permission("setthespawn.sign.use.everybody");
+	public static final Permission STOPALLCOUNT = new Permission("setthespawn.countdown.stopall");
+
+	public static final Permission SPAWNCOST = new Permission(plugin.getConfig().getString("spawn-cost.permission"));
+	public static final Permission SPAWNSIGNCOST = new Permission(plugin.getConfig().getString("spawn-cost.sign.permission"));
+	public static final Permission CANCELTPMOVE = new Permission(plugin.getConfig().getString("cancel-teleport.move.permission"));
+	public static final Permission CANCELTPJUMP = new Permission(plugin.getConfig().getString("cancel-teleport.jump.permission"));
+	public static final Permission CANCELTPDAMAGE = new Permission(plugin.getConfig().getString("cancel-teleport.damage.permission"));
 
 }
