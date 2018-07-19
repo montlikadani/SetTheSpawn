ae475616484f2a84791b2363b47ff674acecf829
montlikadani <montlikada@gmail.com>
2018-07-19 11:15:37 +0200

    Added no damage time interval
    Other:
    - Added disable spawn in pvp functions. - To disable spawn command when you attack a player or attack you.
    - Removed enable-message booleans. - If the string is empty does not send the message.
    - Added paying functions to move, jump & damage.
    - Added launch firework functions.
    - Now the GUI lore is string list.
    - Added enable-gui boolean.

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # SetTheSpawn
+
+[![bStats](https://img.shields.io/badge/bStats-1.1-brightgreen.svg)](https://bstats.org/plugin/bukkit/SetTheSpawn)
+
+## Link
+* [Spigot](https://www.spigotmc.org/resources/%E2%A4%A7-setthespawn-%E2%A4%A7.48992/)
+* [Bukkit](https://dev.bukkit.org/projects/setthespawn)
+* [MCMarket](https://www.mc-market.org/resources/5799/)
+
+### Other
 If you find a bug, please write here: https://github.com/montlikadani/SetTheSpawn/issues
-
-__Pull Request can be anybody with any function. (Except source code)__ [PR](https://github.com/montlikadani/SetTheSpawn/pulls)
-
-Website: https://www.spigotmc.org/resources/setthespawn.48992/
 
 Wiki: https://github.com/montlikadani/SetTheSpawn/wiki
 
 Changelog: https://github.com/montlikadani/SetTheSpawn/blob/master/Changelog.txt
 
-# Download
+### Download
 https://github.com/montlikadani/SetTheSpawn/releases
--- src/hu/montlikadani/SetTheSpawn/Permissions.java
+++ src/hu/montlikadani/SetTheSpawn/Permissions.java
@@ -4,7 +4,7 @@ import org.bukkit.permissions.Permission;
 
 public class Permissions {
 
-	public static final SetTheSpawn plugin = SetTheSpawn.getInstance();
+	private static SetTheSpawn plugin = SetTheSpawn.getPlugin(SetTheSpawn.class);
 
 	public static final Permission PINFO = new Permission("setthespawn.plugininfo");
 	public static final Permission HELP = new Permission("setthespawn.help");
@@ -23,9 +23,13 @@ public class Permissions {
 	public static final Permission USESIGN = new Permission("setthespawn.sign.use");
 	public static final Permission SIGNTPALL = new Permission("setthespawn.sign.use.everybody");
 	public static final Permission STOPALLCOUNT = new Permission("setthespawn.countdown.stopall");
+	public static final Permission BYPASSPVP = new Permission("setthespawn.bypass.blockspawninpvp");
 
 	public static final Permission SPAWNCOST = new Permission(plugin.getConfig().getString("spawn-cost.permission"));
 	public static final Permission SPAWNSIGNCOST = new Permission(plugin.getConfig().getString("spawn-cost.sign.permission"));
+	public static final Permission SPAWNMOVEDCOST = new Permission(plugin.getConfig().getString("cancel-teleport.move.pay-if-moved.permission"));
+	public static final Permission SPAWNJUMPEDCOST = new Permission(plugin.getConfig().getString("cancel-teleport.jump.pay-if-jumped.permission"));
+	public static final Permission SPAWNDAMAGECOST = new Permission(plugin.getConfig().getString("cancel-teleport.damage.pay-if-damage.permission"));
 	public static final Permission CANCELTPMOVE = new Permission(plugin.getConfig().getString("cancel-teleport.move.permission"));
 	public static final Permission CANCELTPJUMP = new Permission(plugin.getConfig().getString("cancel-teleport.jump.permission"));
 	public static final Permission CANCELTPDAMAGE = new Permission(plugin.getConfig().getString("cancel-teleport.damage.permission"));
