0ac1d73bcf5962ece1420a509aa38cda1a173b15
montlikadani <montlikada@gmail.com>
2019-02-09 16:12:09 +0100

    Added sound to cancel teleport
    - Moved some messages to messages.yml file.
    - Changed the teleport-on-command for simpler. (starts with player or console)
    - Make configurable more easier the signs text.
    - Added a option to save signs data to a file.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# SetTheSpawn
+# SetTheSpawn [![GitHub release](https://img.shields.io/github/release/montlikadani/SetTheSpawn.svg)](https://github.com/montlikadani/SetTheSpawn/releases) [![Github All Releases](https://img.shields.io/github/downloads/montlikadani/RageMode/total.svg)](https://github.com/montlikadani/RageMode/releases) [![GitHub issues](https://img.shields.io/github/issues/montlikadani/SetTheSpawn.svg)](https://github.com/montlikadani/SetTheSpawn/issues)
 
-[![bStats](https://img.shields.io/badge/bStats-1.1-brightgreen.svg)](https://bstats.org/plugin/bukkit/SetTheSpawn)
+[![bStats](https://img.shields.io/badge/bStats-1.4-brightgreen.svg)](https://bstats.org/plugin/bukkit/SetTheSpawn)
+
+Set spawn at the location.
+***
+
+A fully configurable plugin that allows the player to teleport to the spawn point with one command, which is configurable a lot of features are usable.
 
 ## Link
-* [Spigot](https://www.spigotmc.org/resources/%E2%A4%A7-setthespawn-%E2%A4%A7.48992/)
+* [Spigot](https://www.spigotmc.org/resources/48992/)
 * [Bukkit](https://dev.bukkit.org/projects/setthespawn)
 * [MCMarket](https://www.mc-market.org/resources/5799/)
 
@@ -13,6 +18,3 @@ If you find a bug, please write here: https://github.com/montlikadani/SetTheSpaw
 Wiki: https://github.com/montlikadani/SetTheSpawn/wiki
 
 Changelog: https://github.com/montlikadani/SetTheSpawn/blob/master/Changelog.txt
-
-### Download
-https://github.com/montlikadani/SetTheSpawn/releases
--- src/hu/montlikadani/SetTheSpawn/Permissions.java
+++ src/hu/montlikadani/SetTheSpawn/Permissions.java
@@ -1,40 +1,52 @@
 package hu.montlikadani.SetTheSpawn;
 
-import org.bukkit.permissions.Permission;
-
 public class Permissions {
 
 	private static SetTheSpawn plugin = SetTheSpawn.getPlugin(SetTheSpawn.class);
 
-	public static final Permission PINFO = new Permission("setthespawn.plugininfo");
-	public static final Permission HELP = new Permission("setthespawn.help");
-	public static final Permission RELOAD = new Permission("setthespawn.reload");
-	public static final Permission PDISABLE = new Permission("setthespawn.plugindisable");
-	public static final Permission TABCOMP = new Permission("setthespawn.cmd.tabcomplete");
-	public static final Permission SETSPAWN = new Permission("setthespawn.setspawn");
-	public static final Permission SPAWNUSE = new Permission("setthespawn.spawn.use");
-	public static final Permission DELSPAWN = new Permission("setthespawn.delspawn");
-	public static final Permission BYPASSCOOLD = new Permission("setthespawn.bypass.cooldown");
-	public static final Permission SPAWNALL = new Permission("setthespawn.spawn.teleport.all");
-	public static final Permission SPAWNOTHER = new Permission("setthespawn.spawn.teleport.other");
-	public static final Permission SETUP = new Permission("setthespawn.spawn.setup");
-	public static final Permission CREATESIGN = new Permission("setthespawn.sign.create");
-	public static final Permission BREAKSIGN = new Permission("setthespawn.sign.break");
-	public static final Permission USESIGN = new Permission("setthespawn.sign.use");
-	public static final Permission SIGNTPALL = new Permission("setthespawn.sign.use.everybody");
-	public static final Permission STOPALLCOUNT = new Permission("setthespawn.countdown.stopall");
-	public static final Permission BYPASSPVP = new Permission("setthespawn.bypass.blockspawninpvp");
-	public static final Permission BUKKITSPAWN = new Permission("setthespawn.bukkitspawn");
-	public static final Permission BUKKITSPAWNOTHER = new Permission("setthespawn.bukkitspawn.other");
-	public static final Permission IMPORTESSSPAWN = new Permission("setthespawn.importessspawn");
+	public enum Perm {
+		PINFO("setthespawn.plugininfo"),
+		HELP("setthespawn.help"),
+		RELOAD("setthespawn.reload"),
+		PDISABLE("setthespawn.disable"),
+		SETSPAWN("setthespawn.setspawn"),
+		SPAWNUSE("setthespawn.spawn.use"),
+		DELSPAWN("setthespawn.delspawn"),
+		BYPASSCOOLD("setthespawn.bypass.cooldown"),
+		SPAWNALL("setthespawn.spawn.teleport.all"),
+		SPAWNOTHER("setthespawn.spawn.teleport.other"),
+		SETUP("setthespawn.spawn.setup"),
+		CREATESIGN("setthespawn.sign.create"),
+		BREAKSIGN("setthespawn.sign.break"),
+		USESIGN("setthespawn.sign.use"),
+		SIGNTPALL("setthespawn.sign.use.everybody"),
+		STOPALLCOUNT("setthespawn.countdown.stopall"),
+		BYPASSPVP("setthespawn.bypass.blockspawninpvp"),
+		BUKKITSPAWN("setthespawn.bukkitspawn"),
+		BUKKITSPAWNOTHER("setthespawn.bukkitspawn.other"),
+		IMPORTESSSPAWN("setthespawn.importessspawn"),
+		TOGGLESPAWN("setthespawn.togglespawn"),
+		BYPASSTOGGLESPAWN("setthespawn.togglespawn.bypass"),
+		BYPASSFLY("setthespawn.bypass.fly"),
+		BYPASSSNEAK("setthespawn.bypass.sneak"),
 
-	public static final Permission SPAWNCOST = new Permission(plugin.getConfig().getString("spawn-cost.permission"));
-	public static final Permission SPAWNSIGNCOST = new Permission(plugin.getConfig().getString("spawn-cost.sign.permission"));
-	public static final Permission SPAWNMOVEDCOST = new Permission(plugin.getConfig().getString("cancel-teleport.move.pay-if-moved.permission"));
-	public static final Permission SPAWNJUMPEDCOST = new Permission(plugin.getConfig().getString("cancel-teleport.jump.pay-if-jumped.permission"));
-	public static final Permission SPAWNDAMAGECOST = new Permission(plugin.getConfig().getString("cancel-teleport.damage.pay-if-damage.permission"));
-	public static final Permission CANCELTPMOVE = new Permission(plugin.getConfig().getString("cancel-teleport.move.permission"));
-	public static final Permission CANCELTPJUMP = new Permission(plugin.getConfig().getString("cancel-teleport.jump.permission"));
-	public static final Permission CANCELTPDAMAGE = new Permission(plugin.getConfig().getString("cancel-teleport.damage.permission"));
+		SPAWNCOST(plugin.getConfig().getString("spawn-cost.permission")),
+		SPAWNSIGNCOST(plugin.getConfig().getString("spawn-cost.sign.permission")),
+		SPAWNMOVEDCOST(plugin.getConfig().getString("cancel-teleport.move.pay-if-moved.permission")),
+		SPAWNJUMPEDCOST(plugin.getConfig().getString("cancel-teleport.jump.pay-if-jumped.permission")),
+		SPAWNDAMAGECOST(plugin.getConfig().getString("cancel-teleport.damage.pay-if-damage.permission")),
+		CANCELTPMOVE(plugin.getConfig().getString("cancel-teleport.move.permission")),
+		CANCELTPJUMP(plugin.getConfig().getString("cancel-teleport.jump.permission")),
+		CANCELTPDAMAGE(plugin.getConfig().getString("cancel-teleport.damage.permission"));
 
+		private String perm;
+
+		Perm(String perm) {
+			this.perm = perm;
+		}
+
+		public String getPerm() {
+			return perm;
+		}
+	}
 }
